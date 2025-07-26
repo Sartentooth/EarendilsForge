b423b089c57f52b75383cb2fa6b0d53ce6464ee5
Sartentooth <sebastiancascallares@gmail.com>
2025-07-26 13:58:18 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
   base: '/EarendilsForge/',
   output: 'static',
-
-  integrations: [react()]
+  integrations: [
+    react(),
+    tailwind()
+  ]
 });
