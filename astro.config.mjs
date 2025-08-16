0d6ed3bb28efcbd672dd39164c48609ca14bcffd
Sartentooth <sebastiancascallares@gmail.com>
2025-08-16 13:38:46 -0300

    astro config fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-  base: '/EarendilsForge/',
+  base: isGitHubPages ? '/EarendilsForge/' : '/',
   output: 'static',
   integrations: [
     react(),
